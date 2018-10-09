88f7b47b4744fc95cb275a598391fea06389d573
XiaoyiWang98 <seanwang20121997@hotmail.com>
2018-10-09 11:19:50 -0400

    Mouth bug fixed

---
+++ train_model.py
@@ -0,0 +1,80 @@
+# train the model using conditional Gaussian Classifiers
+
+# Siqi Dai
+# Oct 8, 2018
+
+import glob
+import cv2
+import math
+
+
+def train_model():
+    train_path = './Samples/train/'
+    validation_path = './Samples/validation/'
+
+    imgset_train_up = glob.glob(train_path + 'up/' + '*.jpg')
+    imgset_train_down = glob.glob(train_path + 'down/' + '*.jpg')
+    imgset_train_left = glob.glob(train_path + 'left/' + '*.jpg')
+    imgset_train_right = glob.glob(train_path + 'right/' + '*.jpg')
+    imgset_train_middle = glob.glob(train_path + 'middle/' + '*.jpg')
+    imgset_train_click = glob.glob(train_path + 'click/' + '*.jpg')
+
+    # access the pixel values and the total number of images in the respective folders
+    pixel_train_up = accessImgPixelVal(imgset_train_up)
+    pixel_train_down = accessImgPixelVal(imgset_train_down)
+    pixel_train_left = accessImgPixelVal(imgset_train_left)
+    pixel_train_right = accessImgPixelVal(imgset_train_right)
+    pixel_train_middle = accessImgPixelVal(imgset_train_middle)
+    pixel_train_click = accessImgPixelVal(imgset_train_click)
+
+    train_data = [pixel_train_up, pixel_train_down, pixel_train_left, pixel_train_right, pixel_train_middle, pixel_train_click]
+    miu_list = computMiu(train_data)
+    theta = computeTheta(train_data, miu_list) # shared vector theta (pixel noise standard deviation)
+
+    return miu_list, theta
+
+
+def accessImgPixelVal(imgset):
+    list = []
+    for im in imgset:
+        img = cv2.imread(im)
+        row, col = img.shape[0], img.shape[1]
+        pixel_vals = []
+        for i in range(0,row):
+            for j in range(0,col):
+                pixel_vals.append(img[i, j][0]) # only focus on red channel here
+        list.append(pixel_vals)
+    return list
+
+
+# compute the vector miu
+def computMiu(train_data):
+    miu_list = []
+    for k in range(0,6): # six classes
+        num_of_attribute = len(train_data[k][0])
+        num_imgs = len(train_data[k])
+        miu = []
+        for i in range(0, num_of_attribute):
+            x_kji = 0;
+            for j in range(num_imgs):
+                x_kji = x_kji + train_data[k][j][i]
+            miu.append(x_kji / num_imgs)
+        miu_list.append(miu)
+    return miu_list
+
+
+# compute the shared vector theta (pixel noise standard deviation)
+def computeTheta(train_data, miu_list):
+    sum = 0
+    train_images_per_class = 2 # this will be changed! should be at least 100 train images in each folder
+    num_of_attribute = len(train_data[0][0])
+    for k in range(0, 6):
+        for j in range (0, train_images_per_class):
+            for i in range(0, num_of_attribute):
+                sum = sum + (train_data[k][j][i] - miu_list[k][i])**2
+    theta = math.sqrt(sum/num_of_attribute/train_images_per_class)
+    return theta
+
+
+miu_list, theta = train_model()
+
---
+++ validate_model.py
@@ -0,0 +1,73 @@
+# predict the output using conditional Gaussian Classifiers
+
+# Siqi Dai
+# Oct 8, 2018
+
+
+from train_model import miu_list, theta, accessImgPixelVal
+import glob
+import math
+
+def validate():
+    print('Prediction results:')
+    validation_path = './Samples/validation/'
+
+    imgset_valid_up = glob.glob(validation_path + 'up/' + '*.jpg')
+    imgset_valid_down = glob.glob(validation_path + 'down/' + '*.jpg')
+    imgset_valid_left = glob.glob(validation_path + 'left/' + '*.jpg')
+    imgset_valid_right = glob.glob(validation_path + 'right/' + '*.jpg')
+    imgset_valid_middle = glob.glob(validation_path + 'middle/' + '*.jpg')
+    imgset_valid_click = glob.glob(validation_path + 'click/' + '*.jpg')
+
+    pixel_valid_up = accessImgPixelVal(imgset_valid_up)
+    pixel_valid_down = accessImgPixelVal(imgset_valid_down)
+    pixel_valid_left = accessImgPixelVal(imgset_valid_left)
+    pixel_valid_right = accessImgPixelVal(imgset_valid_right)
+    pixel_valid_middle = accessImgPixelVal(imgset_valid_middle)
+    pixel_valid_click = accessImgPixelVal(imgset_valid_click)
+
+    validation_data = [pixel_valid_up, pixel_valid_down, pixel_valid_left, pixel_valid_right, pixel_valid_middle, pixel_valid_click]
+
+    test_images_per_class = 2  # this will be changed! should be at least test images in each folder
+    num_of_attribute = len(validation_data[0][0])
+
+    prediction_result = [0, 0, 0, 0, 0, 0]
+    for k in range(0, 6):
+        print('validation file "', className(k), '": ')
+        error_case = 0  # count the number of incorrectly predicted cases
+        for j in range(test_images_per_class):
+            prob = [1, 1, 1, 1, 1, 1]  # probabilities of predicting different classes: up, down, left, right, middle, click
+            for m in range(6):  # 6 possible classes
+                expo = 0
+                for i in range(0, num_of_attribute):
+                    expo = expo + int(validation_data[k][j][i] - miu_list[m][i])**2
+                expo = (-1/(2*(int(theta)**2))) * expo
+                prob[m] = math.pow(2*math.pi*theta**2, -num_of_attribute/200) * math.exp(expo) / 6
+
+            max_idx = prob.index(max(prob))  # max_idx is the prediction result
+            print('  ', className(max_idx))
+            if(max_idx != k):
+                error_case = error_case + 1
+        prediction_result[k] = error_case
+        print('===================')
+    print('Number of error cases for up, down, left, right, middle, click:', prediction_result)
+
+
+def className(m):
+    if m == 0:
+        return 'up'
+    elif m == 1:
+        return 'down'
+    elif m == 2:
+        return 'left'
+    elif m == 3:
+        return 'right'
+    elif m == 4:
+        return 'middle'
+    else:
+        return 'click'
+
+
+if __name__ == "__main__":
+    validate()
+
